608150798bb60646b215b8bb54af3f2f5e7f9f2c
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2022-07-01 20:08:39 +0530

    Add command + basic file validation

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
-const { readFileSync } = require('fs');
+const { validate } = require('jsonschema');
+const { readFileSync, read } = require('fs');
 const { join } = require('path');
+const { lookup } = require('mime-types');
+const chalk = require('chalk');
+const { exit } = require('process');
+
+const error = chalk.bold.red;
+const warning = chalk.yellow;
 
 const meta = JSON.parse(readFileSync(join(__dirname, 'package.json')));
+const schema = JSON.parse(readFileSync(join(__dirname, 'schema.json')));
+
 const program = new Command();
 
 program
   .name(meta.name)
   .description(meta.description)
   .version(meta.version);
+
+program.command('bundle')
+  .description('Bundle a valid Relink extension for users')
+  .argument('<file>', 'JSON file that is a valid Relink extension')
+  .action((file) => {
+    let extension;
+
+    try {
+      if (lookup(file) !== 'application/json') {
+        throw Error('File must be JSON');
+      }
+
+      let error = false;
+
+      try {
+        extension = readFileSync(file);
+      } catch {
+        error = true;
+      }
+
+      if (error) throw Error(`File does not exist at path ${file}`);
+
+      try {
+        extension = JSON.parse(extension);
+      } catch {
+        error = true;
+      }
+
+      if (error) throw Error('File is not valid JSON');
+    } catch (e) {
+      console.log(error(e));
+      exit(1);
+    }
+  });
 
 program.parse(process.argv);
 

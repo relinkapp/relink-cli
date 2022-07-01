85ae50a0e2bdc19f324e526ef5bdfb7764a5140f
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2022-07-01 20:19:15 +0530

    Add schema validation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const { Command } = require('commander');
 const { validate } = require('jsonschema');
-const { readFileSync, read } = require('fs');
+const { readFileSync } = require('fs');
 const { join } = require('path');
 const { lookup } = require('mime-types');
 const chalk = require('chalk');
@@ -32,25 +32,35 @@ program.command('bundle')
         throw Error('File must be JSON');
       }
 
-      let error = false;
+      let err = false;
 
       try {
         extension = readFileSync(file);
       } catch {
-        error = true;
+        err = true;
       }
 
-      if (error) throw Error(`File does not exist at path ${file}`);
+      if (err) throw Error(`File does not exist at path ${file}`);
 
       try {
         extension = JSON.parse(extension);
       } catch {
-        error = true;
+        err = true;
       }
 
-      if (error) throw Error('File is not valid JSON');
+      if (err) throw Error('File is not valid JSON');
     } catch (e) {
       console.log(error(e));
+      exit(1);
+    }
+
+    const validation = validate(extension, schema).errors;
+
+    if (validation) {
+      validation.forEach((e) => {
+        console.log(warning(Error(e.stack)));
+      });
+
       exit(1);
     }
   });
